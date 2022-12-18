1de05f9c43a56df4a2564ba30711881cae122c1a
RichardGallagher <78158578+RichardGallagher@users.noreply.github.com>
2022-12-18 20:06:05 +0000

    final

--- src/app/cocktail/cocktail-form/cocktail-form.component.ts
+++ src/app/cocktail/cocktail-form/cocktail-form.component.ts
@@ -13,18 +13,21 @@ export class CocktailFormComponent implements OnInit {
   @Input() cocktail?: Cocktail;
   @Output() cocktailFormClose = new EventEmitter<Cocktail>();
   cocktailForm : FormGroup = new FormGroup({});
+ 
 
   constructor() { }
 
   ngOnInit(): void {
-
+    
     console.table(this.cocktail)
     this.cocktailForm  = new FormGroup({
       drinkName: new FormControl (this.cocktail?.drinkName, [Validators.required, Validators.minLength(2)]),
       ing1: new FormControl (this.cocktail?.ing1, [Validators.required]),
-      measure1: new FormControl(this.cocktail?.measure1),
+      measure1: new FormControl(this.cocktail?.measure1,[Validators.required]),
+      
       })
-  }
+     
+    }
 
 
 onSubmit(){
@@ -39,4 +42,14 @@ closeForm() {
   this.cocktailFormClose.emit(undefined)
 
 }
+
+
+get title() {
+  return this.cocktailForm?.get('drinkName');
+}
+get year_written() {
+  return this.cocktailForm?.get('ing1');
+}
+
+
 }
